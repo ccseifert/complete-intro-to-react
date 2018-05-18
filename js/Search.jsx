f12a6a7af76b43f79a840185a68ad58b3a43d849
Unknown <ccseifert@gmail.com>
2018-05-18 10:25:04 -0400

    State

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: "",
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            placeholder="Search"
+          />
+        </header>
+        <div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+      </div>
+    );
+  }
+}
 
 export default Search;
